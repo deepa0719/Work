f4d97a3192bf824b671113342da14f8bd2826530
Deepa <58700597+deepa0719@users.noreply.github.com>
2022-02-09 23:26:27 +0530

    Update Remove nth Node from End of List.py

--- DSA/Remove nth Node from End of List.py
+++ DSA/Remove nth Node from End of List.py
@@ -1,3 +1,6 @@
+#Time Complexity : O(N), where, N is the number of nodes in the given list.
+#Space Complexity : O(1), since only constant space is used.
+
 # Definition for singly-linked list.
  class ListNode:
      def __init__(self, val=0, next=None):
