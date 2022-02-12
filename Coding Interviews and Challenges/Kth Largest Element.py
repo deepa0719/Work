032a6799cea56a70c729f21865ced94cbc052f1e
Deepa <58700597+deepa0719@users.noreply.github.com>
2022-02-12 12:38:40 +0530

    Update Kth Largest Element.py
    Given an array arr, and an integer k, find the kth largest element.

--- Coding Interviews and Challenges/Kth Largest Element.py
+++ Coding Interviews and Challenges/Kth Largest Element.py
@@ -1,1 +1,20 @@
+#First Method
+def kLarge(arr,k):
+    arr.sort(reverse=True)
+    return arr[k-1]
 
+#Second Method
+def kLarge(arr,k):
+    for i in range(k-1):
+        arr.remove(max(arr))
+    return max(arr)
+  
+#Third Method (using Heaps)
+import heapq
+
+def kLarge(arr, k):
+    arr = [-ele for ele in arr]
+    heapq.heapify(arr)
+    for i in range(k-1):
+        heapq.heappop(arr)
+    return -heapq.heappop(arr)
