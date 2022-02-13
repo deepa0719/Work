c05e5cd48623457297902a613c4acbe4b6b967bf
mabaedk75 <58700597+mabaedk75@users.noreply.github.com>
2022-02-13 14:02:05 +0530

    commit 13/02/2022

--- JavaScript/Blackjack/index.js
+++ JavaScript/Blackjack/index.js
@@ -1,14 +1,19 @@
-let firstCard = getRandomCard()
-let secondCard = getRandomCard()
-let sum = firstCard + secondCard
-let cards = [firstCard, secondCard]
+let cards = []
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
+let sum = 0
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
-// 2. Store the cards paragraph in a variable called cardsEl
 let cardsEl = document.getElementById("cards-el")
+
+let player ={
+    name: "Elixir",
+    chips: 145
+}
+
+let playerEl = document.getElementById("player-el")
+playerEl.textContent = player.name + ": $" + player.chips
 
 function getRandomCard() {
     let randomNum = Math.floor(Math.random()*13)+1 //1 - 13
@@ -24,6 +29,11 @@ function getRandomCard() {
 }
 
 function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    sum = firstCard + secondCard
+    cards= [firstCard, secondCard]
     renderGame()
 }
 
@@ -47,8 +57,10 @@ function renderGame() {
 }
 
 function newCard(){
+    if (isAlive===true && hasBlackJack === false) {
     let card = getRandomCard() 
     sum += card
     cards.push(card)
     renderGame()
+    }
 }
