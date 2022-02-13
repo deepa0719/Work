6620283810cf9a7894306f1e3e9807466d5450fc
mabaedk75 <58700597+mabaedk75@users.noreply.github.com>
2022-02-13 11:32:47 +0530

    commit 2

--- JavaScript/Blackjack/index.js
+++ JavaScript/Blackjack/index.js
@@ -1,7 +1,35 @@
-if (sum < 21) {
-    console.log("Do you want to draw a new card? 🙂")
-} else if (sum === 21) {
-    console.log("Wohoo! You've got Blackjack! 🥳")
-} else if (sum > 21) {
-    console.log("You're out of the game! 😭")
+let firstCard = 10
+let secondCard = 4
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+// 2. Store the cards paragraph in a variable called cardsEl
+let cardsEl = document.getElementById("cards-el")
+
+function startGame() {
+    renderGame()
+}
+
+function renderGame() {
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    messageEl.textContent = message
+}
+
+function newCard(){
+    let card = 7 
+    sum += card
+    renderGame()
 }
