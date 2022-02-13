82cbea430d0222c1cde53417864efaeec95e9c1e
mabaedk75 <58700597+mabaedk75@users.noreply.github.com>
2022-02-13 09:10:57 +0530

    commit 13/02/2022

--- JavaScript/Passenger Counter/index.js
+++ JavaScript/Passenger Counter/index.js
@@ -3,12 +3,16 @@
 //chnage the count-el in the HTML to reflect the new count
 
 let countEl = document.getElementById("count-el") //passing in arguments
-
-console.log(countEl)
-
 let count = 0
+let saveEl = document.getElementById("save-el")
 
 function increment() {
     count = count+1
     countEl.innerText = count
+}
+
+function save() {
+    let display = " " + count + " - "
+    saveEl.textContent += display
+    console.log(count)
 }
