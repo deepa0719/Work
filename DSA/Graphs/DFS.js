4f0e6da948dc1f45e5cef4a4429e7611c1ebbfb1
mabaedk75 <58700597+mabaedk75@users.noreply.github.com>
2022-02-15 22:28:59 +0530

    final commit

--- DSA/Graphs/DFS.js
+++ DSA/Graphs/DFS.js
@@ -1,4 +1,5 @@
 const depthFirstPrint = (graph, source) => {
+    //Iterative Way
     const stack = [ source ];
 
     while (stack.length > 0) {
@@ -9,19 +10,22 @@ const depthFirstPrint = (graph, source) => {
             stack.push(neighbor)
         }
     }
+
+
+    //Recursive Way
+    // console.log(source);
+    // for (let neighbor of graph[source]) {
+    //     depthFirstPrint(graph, neighbor);
+    // }
 };
 
 const graph = {
     a: ['b', 'c'],
-    b: ['d', 'g'],
-    c: ['e', 'j'],
+    b: ['d'],
+    c: ['e'],
     d: ['f'],
-    e: ['h', 'i'],
-    f: [],
-    g: [],
-    h: [],
-    i: [],
-    j: []
+    e: [],
+    f: []
 };
 
-depthFirstPrint(graph, 'a'); //acjeihbgdf or abgdfcjeih
+depthFirstPrint(graph, 'a');
