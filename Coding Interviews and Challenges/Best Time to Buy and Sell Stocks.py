3a203d37f45a587529e654e927214a0aadd33c9a
Deepa <58700597+deepa0719@users.noreply.github.com>
2022-02-21 10:22:18 +0530

    LeetCode 121

--- Coding Interviews and Challenges/Best Time to Buy and Sell Stocks.py
+++ Coding Interviews and Challenges/Best Time to Buy and Sell Stocks.py
@@ -1,9 +1,9 @@
 # You are given an array prices where prices[i] is the price of a given stock on the ith day.
-
 # You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
-
 # Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
 
+
+#Method 1
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         max_profit, min_price = 0, float('inf')
@@ -12,4 +12,18 @@ class Solution:
             profit = price - min_price
             max_profit = max(max_profit, profit)
         return max_profit
-                    
+    
+#Method 2: Sliding Window
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        l, r = 0, 1 #left and right pointers
+        maxP = 0
+        while r < len(prices):
+            if prices[l] < prices[r]:
+                profit = prices[r] - prices[l]
+                maxP = max(maxP, profit)
+            #when prices[r] > prices[l], shift l
+            else:
+                l = r
+            r+=1 #keep moving r to the right
+        return maxP
