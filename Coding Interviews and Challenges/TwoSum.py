90a5fab45c627537f7585ac9d2db71d6f0e0141e
Deepa <58700597+deepa0719@users.noreply.github.com>
2022-02-21 13:01:01 +0530

    LeetCode 1

---
+++ Coding Interviews and Challenges/TwoSum.py
@@ -0,0 +1,14 @@
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+# You may assume that each input would have exactly one solution, and you may not use the same element twice. You can return the answer in any order.
+
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(len(nums)):
+            if target - nums[i] in nums and i != nums.index(target-nums[i]):
+                return [i, nums.index(target-nums[i])]
